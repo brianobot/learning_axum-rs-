49c4fca1ecdc1b0b388a07038f9526cec38b7385
Brian Obot <brianobot@gmail.com>
2025-03-11 13:09:53 +0100

    Initial commit

--- hello-world/src/main.rs
+++ hello-world/src/main.rs
@@ -1,25 +1,41 @@
 use axum::{routing::get, Router};
+use axum::response::Json;
 
+use tracing::info;
+use tracing_subscriber;
+use serde_json::{json, Value};
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new()
-        .route("/", get(hello))
+    tracing_subscriber::fmt()
+        .compact()
+        .init();
+
+    let app = Router::<()>::new()
+        .route("/", get(hello)) 
+        // each route has some number of functions that relate to a http request verb
+        // for multiple verbs chain those functions with the dot notation on the previous supported verbs
+        // route("/blogs", get(blog).post(blog))
         .route("/bye", get(bye))
-        .route("/profile", get(|| async { "Profile Page!" }))
-        .route("/info", get(|| async { "Info Page!" }))
-        .route("/contact-us", get(|| async { "Contact Us Page!" }))
-        .route("/about", get(|| async { "About Page!" }));
+        .route("/json", get(json_handler));
 
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
 
+
+// whatever our handler function produces as output needs to implement the IntoResponse Trait
 async fn hello() -> &'static str {
     "Hello world!"
 }
 
+
 async fn bye() -> String {
     String::from("Bye")
+}
+
+async fn json_handler() -> Json<Value> {
+    info!("handling json request");
+    Json(json!({"message": "hello internet"}))
 }
